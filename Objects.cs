eb26af6884ee12c3e35b7396c7bf9385eb8cd431
Hamjoshua <biryukov-10@list.ru>
2024-03-03 14:56:44 +0700

    Логика приложения +файлы для примера, словарь

---
+++ Objects.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace app
+{
+    public class FileChanger
+    {
+        private const string _phoneRegex = @"\(\S*\) +\S*";
+        private string _fileText;
+        private string _filePath;
+        private Dictionary<string, List<string>> _wrongWordsDict;
+
+        private static FileChanger s_instance;
+
+        FileChanger()
+        {
+
+        }
+
+        public static FileChanger Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    s_instance = new FileChanger();
+                }
+                return s_instance;
+            }
+        }
+
+        public void SetDictionary(string dictionaryPath)
+        {
+            using (StreamReader streamReader = new StreamReader(dictionaryPath))
+            {
+                string allText = streamReader.ReadToEnd();
+                _wrongWordsDict = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(allText);
+            }
+        }
+
+        public void SetFile(string filePath)
+        {
+            _filePath = filePath;
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                _fileText = streamReader.ReadToEnd();
+            }
+        }
+
+        public int ChangeTelephoneNumbers()
+        {
+            int countOfChanges = 0;
+
+            foreach (Match match in Regex.Matches(_fileText, _phoneRegex))
+            {
+                string foundedString = match.Value;
+                foundedString = foundedString.Replace('-', ' ');
+                foundedString = foundedString.Replace("(", String.Empty);
+                foundedString = foundedString.Replace(")", String.Empty);
+                foundedString = "+380 " + foundedString;
+                _fileText = _fileText.Replace(match.Value, foundedString);
+                ++countOfChanges;
+            }
+
+            return countOfChanges;
+        }
+
+        public int CorrectWrongWords()
+        {
+            int countOfChanges = 0;
+
+            foreach (string key in _wrongWordsDict.Keys)
+            {
+                List<string> values = _wrongWordsDict[key];
+                foreach (string value in values)
+                {
+                    foreach (Match match in Regex.Matches(_fileText, value))
+                    {
+                        _fileText = _fileText.Replace(value, key);
+                        ++countOfChanges;
+                    }
+
+                }
+            }
+
+            return countOfChanges;
+        }
+
+        public void SaveChanges(bool overwrite = false)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(_filePath, !overwrite))
+            {
+                streamWriter.Write(_fileText);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dictionaryPath = "Dictionary.json";
+            DirectoryInfo directoryInfo = null;
+            bool directoryTyped = false;
+
+            FileChanger.Instance.SetDictionary(dictionaryPath);
+
+            while (!directoryTyped)
+            {
+                Console.WriteLine("Введите полный путь до папки: ");
+                string directoryPath = Console.ReadLine();
+                directoryInfo = new DirectoryInfo(directoryPath);
+                if (directoryInfo.Exists)
+                {
+                    directoryTyped = true;
+                }
+                else
+                {
+                    Console.WriteLine("\a Введено неверное имя папки!");
+                }
+            }
+
+            foreach (FileInfo fileInfo in directoryInfo.GetFiles())
+            {
+                if (fileInfo.Extension == ".txt")
+                {
+                    FileChanger.Instance.SetFile(fileInfo.FullName);
+                    int countOfChangedWrongWords = FileChanger.Instance.CorrectWrongWords();
+                    int countOfChangedPhones = FileChanger.Instance.ChangeTelephoneNumbers();
+                    FileChanger.Instance.SaveChanges();
+
+                    Console.WriteLine($"Файл \"{fileInfo.FullName}\"\n-> кол-во исправленных слов: {countOfChangedWrongWords};" +
+                        $" кол-во исправленных телефонов: {countOfChangedPhones}");
+                }
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
